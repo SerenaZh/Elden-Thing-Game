36e8f735b5b07fa870bcf5d77af7570cafd81a05
Serena Zhou <szho0097@student.monash.edu>
2025-06-02 11:27:21 +1000

    New Actions - Added new Shovel Action and new Water Action to be implemented

---
+++ src/game/actions/ShovelAction.java
@@ -0,0 +1,17 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class ShovelAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return "";
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "";
+    }
+}
