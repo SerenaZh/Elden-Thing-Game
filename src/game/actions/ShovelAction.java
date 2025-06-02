41e87be575a1f553f27fc3f5cf64a5e79f14c7af
Serena Zhou <szho0097@student.monash.edu>
2025-06-02 11:38:33 +1000

    Implemented all basic functionalities for REQ - Added implementation for the two low level plants (Lilac and Daisy) - Added basic implementation for Shovel Action - Added Thirsty interface

--- src/game/actions/ShovelAction.java
+++ src/game/actions/ShovelAction.java
@@ -3,15 +3,17 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.ground.Hole;
 
 public class ShovelAction extends Action {
     @Override
     public String execute(Actor actor, GameMap map) {
-        return "";
+        map.locationOf(actor).setGround(new Hole());
+        return actor.toString() + " has dug a new hole!";
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return "";
+        return actor.toString() + " digs a hole";
     }
 }
--- src/game/ground/plants/Daisy.java
+++ src/game/ground/plants/Daisy.java
@@ -3,13 +3,36 @@ package game.ground.plants;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 import game.ground.Plant;
+import game.ground.Soil;
 
-public class Daisy extends Ground implements Plant {
+public class Daisy extends Ground implements Plant, Thirsty {
+    private int waterLevel = 10;
+
     public Daisy() {super('d', "Daisy");}
 
     @Override
     public boolean applyPlant(Actor actor, GameMap map) {
         return false;
+    }
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        this.decreaseCounter();
+        if (this.waterLevel <= 0) {
+            location.setGround(new Soil());
+        }
+    }
+
+    @Override
+    public void water() {
+
+    }
+
+    @Override
+    public void decreaseCounter() {
+        this.waterLevel -= 1;
     }
 }
