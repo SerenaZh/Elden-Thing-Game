c047022fee3bceca684b95c5757a4902f515c311
Aryan Cyrus <aryan.m10@yahoo.com>
2025-06-02 18:23:37 +1000

    added GiveAction

---
+++ src/game/actions/GiveAction.java
@@ -0,0 +1,42 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.items.Buyable;
+
+/**
+ *
+ */
+
+public class GiveAction extends Action {
+    private Item item;
+    private Actor target;
+    public GiveAction(Actor target, Item item) {
+        this.item = item;
+        this.target = target;
+    }
+
+    /**
+     * Constructor
+     * @param actor that is buying the buyable
+     * @param map map player is.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        actor.removeItemFromInventory(this.item);
+        this.target.addItemToInventory(this.item);
+        return "Farmer gave "+this.item.toString()+" to "+this.target.toString();
+    }
+
+    /**
+     * Shows the text option to be displayed on the terminal
+     * @param actor The actor performing the action.
+     * @return String to be printed
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return "Give " + this.item.toString() + " to " + this.target.toString();
+    }
+}
