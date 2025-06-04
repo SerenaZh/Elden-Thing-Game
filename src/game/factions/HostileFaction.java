1b38595798f2b0dc0edb07ba760eb47b3e24e259
SR060105 <sraj0081@student.monash.edu>
2025-06-04 15:47:22 +1000

    Implemented HostileFaction extending Faction.

--- src/game/factions/HostileFaction.java
+++ src/game/factions/HostileFaction.java
@@ -1,4 +1,13 @@
 package game.factions;
+import game.Capabilities;
+import game.items.weapons.WeaponItem;
 
-public class HostileFaction {
+public class HostileFaction extends Faction {
+    public HostileFaction() {
+        super(Capabilities.HOSTILE);
+    }
+
+    @Override
+    public void factionEffect(WeaponItem weapon) {
+    }
 }
