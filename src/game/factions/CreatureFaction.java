429283a830d9952b11fbabb10dd22e9dba640c34
SR060105 <sraj0081@student.monash.edu>
2025-06-04 15:35:05 +1000

    Implemented the standing logic.

--- src/game/factions/CreatureFaction.java
+++ src/game/factions/CreatureFaction.java
@@ -1,6 +1,11 @@
 package game.factions;
+import game.Capabilities;
 
 
 public class CreatureFaction extends Faction {
-    
+    public CreatureFaction() {
+        super(Capabilities.CREATURE);
+    }
+
+
 }
