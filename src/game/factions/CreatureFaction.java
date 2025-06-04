49af988b70134072df960014655a74c6eb3ec303
SR060105 <sraj0081@student.monash.edu>
2025-06-04 15:36:38 +1000

    Implemented standing reduction when player attacks friendly creatures.

--- src/game/factions/CreatureFaction.java
+++ src/game/factions/CreatureFaction.java
@@ -1,10 +1,15 @@
 package game.factions;
 import game.Capabilities;
-
+import game.items.weapons.WeaponItem;
 
 public class CreatureFaction extends Faction {
     public CreatureFaction() {
         super(Capabilities.CREATURE);
+    }
+
+    @Override
+    public void factionEffect(WeaponItem weapon) {
+        decreaseStanding(1);
     }
 
 
